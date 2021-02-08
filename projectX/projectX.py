b3a1be269c5ef59866b15978eda0324dbc62e9bd
opilane <opilane@HP-CZC6297B9D>
2021-02-08 13:09:31 +0200

    1

--- projectX/projectX.py
+++ projectX/projectX.py
@@ -11,16 +11,10 @@ def image123_ (name):
     img=PhotoImage(file=name).subsample(3)
     can.create_image(20,20,image=img,anchor=NW)
     
-
-
 root=Tk()
-root.geometry("600x600")
+root.geometry("800x800")
 root.title("Elemendid Tkinteris")
-
-#tabs=ttk.Notebook(root)
-#texts=["1","2","3","4","5","6","7","8"]
-
-
+#222
 tabs=ttk.Notebook(root)
 tab1=Frame(tabs)
 tab2=Frame(tabs)
@@ -31,57 +25,44 @@ tabs.add(tab2,text="BMW M3 E46")
 tabs.add(tab3,text="AMG GT")
 tabs.add(tab4,text="RS7")
 
-def funktion(a):
-    tabs.select(a)
-
+texts=""
 def camry():
-    lines = ""
-    with open("Camry.txt", "r") as f:
-        for element in f.readlines():
-            lines+=element.strip()
-        lbl1.configure(text=lines)
+    global texts
+    for text in fileinput.input("Camry.txt"):
+        texts=texts+text
+        lbl1.configure(text=texts)
 
+texts1=""
 def bmw():
-    lines = ""
-    with open("bmw.txt", "r") as f:
-        for element in f.readlines():
-            lines+=element.strip()
-        lbl2.configure(text=lines)
+    global texts1
+    for text in fileinput.input("bmw.txt"):
+        texts1=texts1+text
+        lbl2.configure(text=texts1)
 
+texts2=""
 def amg():
-    lines = ""
-    with open("amg.txt", "r") as f:
-        for element in f.readlines():
-            lines+=element.strip()
-        lbl3.configure(text=lines)
+    global texts2
+    for text in fileinput.input("amg.txt"):
+        texts2=texts2+text
+        lbl3.configure(text=texts2)
 
+texts3=""
 def rs7():
-    lines = ""
-    with open("rs7.txt", "r") as f:
-        for element in f.readlines():
-            lines+=element.strip()
-        lbl4.configure(text=lines)
+    global texts3
+    for text in fileinput.input("rs7.txt"):
+        texts3=texts3+text
+        lbl4.configure(text=texts3)
 
-##########################################################НУжно сделать текст с информацией#########################################################
 M=Menu(root)
 root.config(menu=M)
-
 
 m3=Menu(M,tearoff=0)
 M.add_cascade(label="Машины",menu=m3)
 m3.add_command(label="AMG GT",command=lambda:image123_("amg.png"))
 m3.add_command(label="CAMRY 3.5",command=lambda:image123_("camry.png"))
-m3.add_command(label="BMW e46",command=lambda:image123_("e46.png"))
+m3.add_command(label="BMW E46",command=lambda:image123_("e46.png"))
 m3.add_command(label="AUDI RS7",command=lambda:image123_("rs7.png"))
 m3.add_separator()
-
-m4=Menu(M,tearoff=0)
-M.add_cascade(label="Информация",menu=m3)
-m4.add_command(label="AMG GT")
-m4.add_command(label="CAMRY 3.5")
-m4.add_command(label="BMW e46")
-m4.add_command(label="AUDI RS7")
-m4.add_separator()
 
 can2=Canvas(tab1,width=500,height=400,)
 img2=PhotoImage(file="camry.png").subsample(3)
@@ -103,10 +84,10 @@ img3=PhotoImage(file="rs7.png").subsample(3)
 can3.create_image(20,20,image=img3,anchor=NW)
 btn4=Button(tab4,text="Информация об Авто", command=rs7)
 
-lbl1=Label(tab1,width=80,height=10,bg="lightblue")
-lbl2=Label(tab2,width=80,height=10,bg="lightblue")
-lbl3=Label(tab3,width=80,height=10,bg="lightblue")
-lbl4=Label(tab4,width=80,height=10,bg="lightblue")
+lbl1=Label(tab1,width=100,height=30,bg="lightblue")
+lbl2=Label(tab2,width=100,height=30,bg="lightblue")
+lbl3=Label(tab3,width=100,height=30,bg="lightblue")
+lbl4=Label(tab4,width=100,height=30,bg="lightblue")
 
 can.pack()
 btn1.pack()
